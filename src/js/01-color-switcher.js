82828826b55f22600d4a1a6d229983c9999f80ed
xxx3105 <123968288+xxx3105@users.noreply.github.com>
2023-07-10 23:32:18 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let callChanger = setInterval(colorChanger, 1000)
+
+function colorChanger() {
+let themeColor = getRandomHexColor();
+document.body.style = `background-color: ${themeColor};`;
+}
+
+function getRandomHexColor() {
+return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+    
+
+startBtn.addEventListener('click', () => {
+    callChanger = setInterval(colorChanger, 1000); 
+   
+});
+
+stopBtn.addEventListener('click', () => {
+clearInterval(callChanger);
+startBtn.disabled = false;
+});
+
+
 
