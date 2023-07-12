cfd684c338b490a9ca8ecf1f8a055ba9ad743fa6
xxx3105 <123968288+xxx3105@users.noreply.github.com>
2023-07-12 21:29:43 +0000

    Deploying to gh-pages from @ xxx3105/goit-js-hw-09@3e90c6209bd49679bba85a2d2b1f161949c6121c 🚀

---
+++ 01-color-switcher.10ae1e80.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=setInterval(a,1e3);function a(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style="background-color: ".concat(t,";")}t.addEventListener("click",(function(){n=setInterval(a,1e3),e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.10ae1e80.js.map
